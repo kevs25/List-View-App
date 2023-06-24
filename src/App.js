af243273a8b649228f7b1e056ad410acfd603344
kevs25 <kevindaniel200225@gmail.com>
2023-06-24 15:10:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const [inputValue, setInputValue] = useState("")
+
+  useEffect(() => {
+   
+    fetch('https://reqres.in/api/users?page=2')
+    .then((response) => response.json())
+    .then((data) => {
+     setItems(data.data)
+     console.log("fetch")
+    })
+  },[])
+  
+  
+  
+  const filteredItems = items.filter(item => {
+    return item.first_name.toLowerCase().includes(inputValue.toLowerCase())
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className='search-container'>
+        <input value={inputValue} onChange={e => setInputValue(e.target.value)} type="search" placeholder='search'/>
+      </div>
+      <div>
+        
+      {filteredItems.map(item => {
+        return (
+          <div className='items-container'>
+            <p>{item.id}</p>
+            <div className='image-container'>
+              <img src={item.avatar} alt=''></img>
+            </div>
+            <h4>{item.first_name}</h4>
+          </div>
+        )
+      })}
     </div>
+    </>
   );
 }
 
